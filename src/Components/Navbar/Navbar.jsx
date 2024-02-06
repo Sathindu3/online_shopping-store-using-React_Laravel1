e18ccbec4ad4d6c6a730a48203582b65b88fa457
Sathindu Prabhath <wond3rgaming369@gmail.com>
2024-02-06 20:58:01 +0530

    step2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import reactLogo from "./online_shopping.png";
+import Logo from "./online_shopping.png";
 import cart from "./shopping_cart.png";
 import { Link } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="nav-logo">
-            <img src={reactLogo} alt="react logo" />
+            <img src={Logo} alt="logo" />
             <p>T Shirt mart</p>
         </div>
         <ul className="nav-menu">
