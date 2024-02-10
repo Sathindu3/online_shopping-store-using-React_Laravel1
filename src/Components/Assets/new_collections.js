0ab970171b12b21c0d38e4c45364b029c8b247ed
Sathindu Prabhath <wond3rgaming369@gmail.com>
2024-02-10 16:33:44 +0530

    new collection

---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,30 @@
+import p1_img from './Product_1.png'
+import p2_img from './Product_2.png'
+import p3_img from './Product_3.png'
+
+let data_product = [
+    {
+        id:1,
+        name:"Black tshirt",
+        image:p1_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    {
+        id:2,
+        name:"white tshirt",
+        image:p2_img,
+        new_price:85.00,
+        old_price:120.50,
+    },
+    {
+        id:3,
+        name:"Red tshirt",
+        image:p3_img,
+        new_price:120.00,
+        old_price:165.50,
+    },
+
+];
+
+export default data_product;
