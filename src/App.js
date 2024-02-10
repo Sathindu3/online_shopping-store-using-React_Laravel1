a6c416d9dcbaf4a84134d2a2c4e7ba44cd6630ad
Sathindu Prabhath <wond3rgaming369@gmail.com>
2024-02-10 12:46:15 +0530

    hero page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
-import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
+import Shop from './Pages/Shop';
 import LoginSignup from './Pages/LoginSignup';
 
 function App() {
@@ -12,11 +12,12 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-      <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
         <Route path='/womens' element={<ShopCategory category="womens"/>}/>
         <Route path='/kids' element={<ShopCategory category="kids"/>}/>
         <Route path='/product' element={<Product/>}/>
-          <Route path=':productId' element={<Product/>}/>       
+        <Route path=':productId' element={<Product/>}/>       
         <Route path='/cart' element={<cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
         <Route path='/cart' element={<cart/>}/>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import './Hero.css'
+import hand_icon from './waving-hand.png'
+import arrow_icon from './arrow.png'
+import hero_icon from './model.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-left">
         <h2>New Arrivals only</h2>
+        <div>
+            <div className="hero-hand-icon">
+              <p>New</p>
+              <img src={hand_icon} alt="" />
+            </div>
+            <p>Collections</p>
+            <p>for everybody</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
       </div>
       
       <div className="hero-right">
+        <img src={hero_icon} alt="" />
 
       </div>
 
